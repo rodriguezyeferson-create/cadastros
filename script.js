f23c9c199f6791e590f51537ba1fd5def1819197
rodriguezyeferson-create <rodriguez.yeferson@escola.pr.gov.br>
2026-03-10 10:44:40 -0300

    .

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-body {
-    font-family: Arial, Helvetica, sans-serif;
-    text-align: center;
-    margin-top: 50px;
-    background-color: coral;
-}
-
-input {
-    display: block;
-    margin: 10px auto;
-    padding: 8px;
-}
-
-button {
-    padding: 8px 20px;
-    background-color: chartreuse;
-    border: none;
-}
+function salvar() {
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    console.log("Nome:", nome);
+    console.log("Idade:", idade);
+};
